173f3abc1319cefedce813c498fa29aa87976d26
Clinton Selke <clinuxrulz@gmail.com>
2018-10-07 13:39:41 +1000

    lambda

--- src/sodium/mod.rs
+++ src/sodium/mod.rs
@@ -1,4 +1,12 @@
 pub use self::cell::Cell;
+pub use self::dep::Dep;
+pub use self::lambda::Lambda;
+pub use self::lambda::IsLambda1;
+pub use self::lambda::IsLambda2;
+pub use self::lambda::IsLambda3;
+pub use self::lambda::IsLambda4;
+pub use self::lambda::IsLambda5;
+pub use self::lambda::IsLambda6;
 pub use self::memo_lazy::MemoLazy;
 pub use self::node::Node;
 pub use self::sodium_ctx::SodiumCtx;
@@ -6,7 +14,12 @@ pub use self::sodium_ctx::SodiumCtxData;
 pub use self::sodium_ctx::WeakSodiumCtx;
 
 mod cell;
+mod dep;
 pub mod gc;
+
+#[macro_use]
+mod lambda;
+
 mod memo_lazy;
 mod node;
 mod sodium_ctx;
