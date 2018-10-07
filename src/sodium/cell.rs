b51787651df502e20fdd0541f255e719068b992a
Clinton Selke <clinuxrulz@gmail.com>
2018-10-07 13:34:55 +1000

    Cell::new

--- src/sodium/cell.rs
+++ src/sodium/cell.rs
@@ -1,4 +1,6 @@
+use sodium::gc::Finalize;
 use sodium::gc::Gc;
+use sodium::gc::Trace;
 use sodium::MemoLazy;
 use sodium::Node;
 use sodium::SodiumCtx;
@@ -9,8 +11,17 @@ pub struct Cell<A> {
     node: Node
 }
 
-impl<A> Cell<A> {
-    pub fn new(sodium_ctx: &SodiumCtx, value: A) {
-        
+impl<A: Clone + Trace + Finalize + 'static> Cell<A> {
+    pub fn new(sodium_ctx: &SodiumCtx, value: A) -> Cell<A> {
+        let mut gc_ctx = sodium_ctx.gc_ctx();
+        Cell {
+            value: gc_ctx.new_gc(UnsafeCell::new(MemoLazy::new(move || value.clone()))),
+            node: Node::new(
+                sodium_ctx,
+                || {},
+                Vec::new(),
+                || {}
+            )
+        }
     }
 }
--- src/sodium/memo_lazy.rs
+++ src/sodium/memo_lazy.rs
@@ -1,8 +1,23 @@
+use sodium::gc::Finalize;
+use sodium::gc::GcDep;
+use sodium::gc::Trace;
 use std::cell::UnsafeCell;
 
 pub struct MemoLazy<A> {
     thunk: Box<Fn()->A>,
     val_op: UnsafeCell<Option<A>>
+}
+
+impl<A: Trace> Trace for MemoLazy<A> {
+    fn trace(&self, f: &mut FnMut(&GcDep)) {
+        self.get().trace(f);
+    }
+}
+
+impl<A: Finalize> Finalize for MemoLazy<A> {
+    fn finalize(&mut self) {
+        self.get_mut().finalize();
+    }
 }
 
 impl<A> MemoLazy<A> {
@@ -23,6 +38,20 @@ impl<A> MemoLazy<A> {
                     *self.val_op.get() = Some(val);
                 }
                 self.get()
+            }
+        }
+    }
+
+    pub fn get_mut(&mut self) -> &mut A {
+        let val_op = unsafe { &mut *self.val_op.get() };
+        match val_op {
+            Some(ref mut val) => val,
+            None => {
+                let val = (self.thunk)();
+                unsafe {
+                    *self.val_op.get() = Some(val);
+                }
+                self.get_mut()
             }
         }
     }
