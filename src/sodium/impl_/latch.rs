1de99cf8a245b9ef598ba07580b3133b95d8290d
Clinton Selke <clinuxrulz@gmail.com>
2018-10-28 00:49:26 +1000

    Latch cloning fix

--- src/sodium/impl_/latch.rs
+++ src/sodium/impl_/latch.rs
@@ -1,22 +1,29 @@
 use sodium::gc::Finalize;
 use sodium::gc::GcDep;
 use sodium::gc::Trace;
+use std::cell::UnsafeCell;
 use std::rc::Rc;
 
 pub struct Latch<A> {
-    thunk: Rc<Fn()->A>,
+    data: Rc<UnsafeCell<LatchData<A>>>
+}
+
+struct LatchData<A> {
+    thunk: Box<Fn()->A>,
     val: A
 }
 
 impl<A: Trace> Trace for Latch<A> {
     fn trace(&self, f: &mut FnMut(&GcDep)) {
-        self.val.trace(f);
+        let self_ = unsafe { &*(*self.data).get() };
+        self_.val.trace(f);
     }
 }
 
 impl<A: Finalize> Finalize for Latch<A> {
     fn finalize(&mut self) {
-        self.val.finalize();
+        let self_ = unsafe { &mut *(*self.data).get() };
+        self_.val.finalize();
     }
 }
 
@@ -30,29 +37,35 @@ impl<A> Latch<A> {
     pub fn new<F: Fn()->A + 'static>(thunk: F) -> Latch<A> {
         let val = thunk();
         Latch {
-            thunk: Rc::new(thunk),
-            val
+            data: Rc::new(UnsafeCell::new(
+                LatchData {
+                    thunk: Box::new(thunk),
+                    val
+                }
+            ))
         }
     }
 
     pub fn get(&self) -> &A {
-        &self.val
+        let self_ = unsafe { &*(*self.data).get() };
+        &self_.val
     }
 
     pub fn get_mut(&mut self) -> &mut A {
-        &mut self.val
+        let self_ = unsafe { &mut *(*self.data).get() };
+        &mut self_.val
     }
 
     pub fn reset(&mut self) {
-        self.val = (self.thunk)();
+        let self_ = unsafe { &mut *(*self.data).get() };
+        self_.val = (self_.thunk)();
     }
 }
 
 impl<A: Clone> Clone for Latch<A> {
     fn clone(&self) -> Self {
         Latch {
-            thunk: self.thunk.clone(),
-            val: self.val.clone()
+            data: self.data.clone()
         }
     }
 }
