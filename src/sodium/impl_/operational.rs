e21ca4aadb54af026849c920aa4662a37ec1ffce
Clinton Selke <clinuxrulz@gmail.com>
2018-11-03 12:46:12 +1000

    Operational::updates

--- src/sodium/impl_/operational.rs
+++ src/sodium/impl_/operational.rs
@@ -1,5 +1,9 @@
+use sodium::impl_::gc::Finalize;
+use sodium::impl_::gc::Trace;
 use sodium::impl_::Cell;
 use sodium::impl_::Stream;
+use std::cell::UnsafeCell;
+use std::rc::Rc;
 
 pub struct Operational {}
 
@@ -8,8 +12,26 @@ impl Operational {
         unimplemented!();
     }
 
-    pub fn updates<A>(ca: Cell<A>) -> Stream<A> {
-        unimplemented!();
+    pub fn updates<A: Clone + Trace + Finalize + 'static>(ca: Cell<A>) -> Stream<A> {
+        let sodium_ctx = ca.node.sodium_ctx();
+        let sodium_ctx = &sodium_ctx;
+        let deps = vec![ca.node.clone()];
+        let first = Rc::new(UnsafeCell::new(true));
+        Stream::_new(
+            sodium_ctx,
+            move || {
+                let first = unsafe { &mut *(*first).get() };
+                if *first {
+                    *first = false;
+                    None
+                } else {
+                    let next_value = unsafe { &*(*ca.next_value).get() };
+                    Some(next_value.clone())
+                }
+            },
+            deps,
+            || {}
+        )
     }
 
     pub fn defer<A>(sa: Stream<A>) -> Stream<A> {
