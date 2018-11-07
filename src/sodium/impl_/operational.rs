4899cf54f5ac9ff563fbe5a16cf4c6d84fd02412
Clinton Selke <clinuxrulz@gmail.com>
2018-11-07 11:35:46 +1000

    Operational::defer

--- src/sodium/impl_/operational.rs
+++ src/sodium/impl_/operational.rs
@@ -1,6 +1,9 @@
 use sodium::impl_::gc::Finalize;
+use sodium::impl_::gc::Gc;
 use sodium::impl_::gc::Trace;
 use sodium::impl_::Cell;
+use sodium::impl_::MemoLazy;
+use sodium::impl_::Node;
 use sodium::impl_::Stream;
 use std::cell::UnsafeCell;
 use std::rc::Rc;
@@ -45,8 +48,71 @@ impl Operational {
         )
     }
 
-    pub fn defer<A>(sa: Stream<A>) -> Stream<A> {
-        unimplemented!();
+    pub fn defer<A: Clone + Trace + Finalize + 'static>(sa: Stream<A>) -> Stream<A> {
+        let sodium_ctx = sa.node.sodium_ctx();
+        let sodium_ctx = &sodium_ctx;
+        let mut gc_ctx = sodium_ctx.gc_ctx();
+        let gc_ctx = &mut gc_ctx;
+        let deps = vec![sa.node.clone()];
+        let value: Gc<UnsafeCell<Option<MemoLazy<A>>>> = gc_ctx.new_gc(UnsafeCell::new(None));
+        let sodium_ctx2 = sodium_ctx.clone();
+        let node2;
+        {
+            let value = value.clone();
+            node2 = Node::new(
+                sodium_ctx,
+                move || {
+                    let sodium_ctx = &sodium_ctx2;
+                    let value = value.clone();
+                    sodium_ctx.post(move || {
+                        let value = unsafe { &mut *(*value).get() };
+                        *value = None;
+                    });
+                    true
+                },
+                Vec::new(),
+                Vec::new(),
+                || {}
+            );
+        }
+        let result = Stream {
+            value: value.clone(),
+            node: node2.clone()
+        };
+        let node2_dep = node2.to_dep();
+        let sodium_ctx2 = sodium_ctx.clone();
+        let node1;
+        {
+            let node2 = node2.clone();
+            node1 = Node::new(
+                sodium_ctx,
+                move || {
+                    let sodium_ctx = &sodium_ctx2;
+                    let sodium_ctx2 = sodium_ctx.clone();
+                    let node2 = node2.clone();
+                    let sa_value_op = sa.peek_value();
+                    let value = value.clone();
+                    if let Some(sa_value) = sa_value_op {
+                        sodium_ctx.post(move || {
+                            let sodium_ctx = &sodium_ctx2;
+                            let node2 = node2.clone();
+                            let sa_value = sa_value.clone();
+                            let value = unsafe { &mut *(*value).get() };
+                            sodium_ctx.transaction(|| {
+                                *value = Some(sa_value);
+                                node2.mark_dependents_dirty();
+                            });
+                        });
+                    }
+                    false
+                },
+                vec![node2_dep],
+                deps,
+                || {}
+            );
+        }
+        node2.add_dependencies(vec![node1]);
+        result
     }
 
     pub fn split<C,A>(s: Stream<C>) -> Stream<A>
